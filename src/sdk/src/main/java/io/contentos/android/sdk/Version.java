e01794fee26ce462401bc322114ba000bfe8fc83
daishuo <daishuo@gmail.com>
2019-05-17 15:21:42 +0800

    sdk version v0.0.1

--- src/sdk/src/main/java/io/contentos/android/sdk/Version.java
+++ src/sdk/src/main/java/io/contentos/android/sdk/Version.java
@@ -2,7 +2,6 @@ package io.contentos.android.sdk;
 
 public class Version {
 
-    // compliant with Contentos test-net v0.0.6
-    public static final String SDK_VERSION = "0.0.6";
+    public static final String SDK_VERSION = "0.0.1";
 
 }
